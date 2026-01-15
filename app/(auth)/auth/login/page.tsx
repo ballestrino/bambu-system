b5563e3c3b91ebf4408a1025c7109fbca7db464a
Nacho <nachoballestrino02@gmail.com>
2026-01-15 16:06:16 -0300

    suspense wrap search params page

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -1,8 +1,14 @@
 import LoginForm from '@/components/auth/LoginForm'
 import { authPagesMetadata } from '@/data/metadata/auth-pages-meta-data'
 
+import { Suspense } from 'react'
+
 export const metadata = authPagesMetadata.login
 
 export default function register() {
-  return <LoginForm />
+  return (
+    <Suspense>
+      <LoginForm />
+    </Suspense>
+  )
 }
